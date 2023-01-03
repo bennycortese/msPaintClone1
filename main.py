b52b489ac58c53391c02458401e65172b0442622
benny <bennycortese@gmail.com>
2023-01-03 00:22:35 -0500

    blank screen setup

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 import pygame
 
-# Press the green button in the gutter to run the script.
+
+def setup_screen():
+    size = [800, 600]
+    screen = pygame.display.set_mode(size)
+    screen.fill("white")
+    pygame.display.update()
+    pygame.display.set_caption("BennyPaint")
+    return screen
+
+
+def main_game_loop(screen):
+    playing = True
+    clock = pygame.time.Clock()
+    while playing:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                playing = False
+        screen.fill("white")
+        pygame.display.update()
+
+
 if __name__ == '__main__':
-    print('PyCharm')
+    screen = setup_screen()
+    main_game_loop(screen)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
