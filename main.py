6e0a57a196f6004088947c75d0837f516d7ddbfb
Benny Cortese <bennycortese@gmail.com>
2023-01-03 00:09:27 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import pygame
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
